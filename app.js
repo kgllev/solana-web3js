87fe4d4416fc783a69426b957293379d02dd5feb
Erick Onyango <erickonyango@Ericks-MacBook-Pro.local>
2022-04-17 22:08:47 +0300

    get wallet balance

---
+++ app.js
@@ -0,0 +1,29 @@
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL
+} = require('@solana/web3.js')
+
+const wallet = new Keypair()
+
+const publicKey = new  PublicKey(wallet._keypair.publicKey)
+const secretKey = wallet._keypair.secretKey
+
+const getWalletBalance = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const walletBalance = await connection.getBalance(publicKey)
+        console.log(walletBalance)
+    }catch (error){
+        console.log(error)
+    }
+}
+
+const main = async() => {
+    await getWalletBalance()
+
+}
+
+main()
